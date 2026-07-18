7580a7277cfb7aa5c589d207d587008dc935ba8a
Ammyzapyty <achirayasaikhamwong@gmail.com>
2026-07-18 12:55:58 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,14 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    # ข้อความที่จะแสดงเมื่อเปิดหน้าเว็บ
-    return "✅ Bot is running properly!"
+    return "Bot is alive!"
 
 def run():
-    # ให้ Flask รันบนพอร์ต 8080 (หรือพอร์ตที่ Render กำหนด)
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    # สร้าง Thread เพื่อให้เว็บเซิร์ฟเวอร์ทำงานพร้อมกับบอทได้โดยไม่บล็อกการทำงานกัน
     t = Thread(target=run)
     t.start()
